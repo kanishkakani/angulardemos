36023b290f239ee55196fcf31a754df37930e36b
kanishkakani <109128177+kanishkakani@users.noreply.github.com>
2022-09-19 16:20:51 +0530

    changes

--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -6,13 +6,29 @@ import { ServerComponent } from './server/server.component';
 import { ProductsComponent } from './products/products.component';
 import { ProductComponent } from './products/product/product.component';
 import { FormsModule } from '@angular/forms';
+import { EcommComponent } from './ecomm/ecomm.component';
+import { CartComponent } from './ecomm/cart/cart.component';
+import { EcomproductComponent } from './ecomm/ecomproduct/ecomproduct.component';
+import { NumbersComponent } from './numbers/numbers.component';
+import { BasicHighlightDirective } from './BasicHighlightDirective';
+import { AccountsComponent } from './accounts/accounts.component';
+import { AccountComponent } from './accounts/account/account.component';
+import { NewaccountComponent } from './accounts/newaccount/newaccount.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ProductsComponent,
-    ProductComponent
+    ProductComponent,
+    EcommComponent,
+    CartComponent,
+    EcomproductComponent,
+    NumbersComponent,
+    BasicHighlightDirective,
+    AccountsComponent,
+    AccountComponent,
+    NewaccountComponent,
   ],
   imports: [
     BrowserModule ,FormsModule],
