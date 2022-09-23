50680cdd6760f927650710228efb72a0c3a3c50b
kanishkakani <109128177+kanishkakani@users.noreply.github.com>
2022-09-23 17:41:02 +0530

    changes

--- myapp/src/app/posts/posts.component.ts
+++ myapp/src/app/posts/posts.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { map } from 'rxjs/operators';
 import { PostData } from './postdata.model';
@@ -7,48 +7,50 @@ import { PostData } from './postdata.model';
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.css']
+  styleUrls: ['./posts.component.css'],
 })
 export class PostsComponent implements OnInit {
   fetchedPosts: PostData[] = [];
-  constructor(private http: HttpClient) { }
+  backendurl = 'http://localhost:3000/posts';
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.fetchPosts();
   }
-  createPost(postData: { title: string; content: string }, form: NgForm) {
-    console.log(postData);
-    this.http
-      .post('http://localhost:3000/posts', postData)
-      .subscribe((responseData) => {
-        console.log(responseData);
-      });
+
+  onCreatePost(postData: { title: string; content: string }, form: NgForm) {
+    this.http.post(this.backendurl, postData).subscribe((responseData) => {
+      console.log(responseData);
+      this.fetchPosts();
+      form.reset();
+    });
   }
   onFetchPosts() {
     this.fetchPosts();
   }
 
-  onDeletePosts() {
-    console.log('delete clicked');
+  onDeletePost(id: number) {
+    this.http.delete(this.backendurl + '/' + id).subscribe((response) => {
+      console.log('Post deleted: ' + response);
+      this.fetchPosts();
+    });
   }
 
   fetchPosts() {
     this.http
-      .get('http://localhost:3000/posts')
+      .get(this.backendurl)
       .pipe(
         map((responseData) => {
           const postsArray: PostData[] = [];
-          // console.log(responseData);
           for (const key in responseData) {
-            postsArray.push({ ...responseData[key], id: key });
+            var x = { ...responseData[key] };
+            postsArray.push(x);
           }
-          console.log(postsArray);
+          return postsArray;
         })
       )
       .subscribe((posts) => {
-        // console.log(posts);
-        // this.fetchedPosts = posts;
+        this.fetchedPosts = posts;
       });
   }
-
 }
